68f42c77672b1595aaabf8e00a953b0a21b31acd
Syed Talha Ahmed <41064991+Talhaverse@users.noreply.github.com>
2024-03-20 17:11:25 +0500

    adding react with inertia

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -4,15 +4,23 @@ namespace App\Http\Controllers;
 
 use App\Models\Chirp;
 use Illuminate\Http\Request;
+// use Illuminate\Http\Response;
+use Inertia\Inertia;
+use Inertia\Response;
+
 
 class ChirpController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(): Response
     {
         //
+        // return response("hello,world.");
+        return Inertia::render('Chirps/Index',[
+            //
+        ]);
     }
 
     /**
