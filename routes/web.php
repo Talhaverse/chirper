98defc3ed6ee70153bb2b6fc2733ce82a4af0391
Syed Talha Ahmed <41064991+Talhaverse@users.noreply.github.com>
2024-03-29 03:22:03 +0500

    learning from fasih

--- routes/web.php
+++ routes/web.php
@@ -15,17 +15,19 @@ Route::get('/', function () {
 });
 
 Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
+
+    return Inertia::render('');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-// Route::middleware('auth')->group(function () {
-//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-// });
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
 Route::resource('chirps',ChirpController::class)
 ->only(['index','store'])
 ->middleware(['auth','verified']);
 
 require __DIR__.'/auth.php';
+
